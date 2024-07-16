aa4df0052c7d2ff982eef4f919882739af571b4c
ibrasow5 <111022844+ibrasow5@users.noreply.github.com>
2024-07-16 15:34:38 +0000

    Commit

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Logo from '../Tola5.png';
 
 function QuestionDetail() {
   const { id } = useParams();
@@ -17,18 +18,29 @@ function QuestionDetail() {
   }
 
   return (
-    <div>
-      <h2>{question.title}</h2>
-      <p>{question.body}</p>
-      <h3>Réponses :</h3>
-      <ul>
-        {question.answers.map(answer => (
-          <li key={answer.id}>
-            <p>{answer.body}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <header className="home-header">
+        <div className="logo-container">
+          <img src={Logo} alt="Logo Tola" className="logo" width="100" height="auto" />
+        </div>
+      </header>
+      <div className="dashboard-main">
+        <div className="question-container">
+          <h2 className="question-title">{question.title}</h2>
+          <p className="question-body">{question.body}</p>
+        </div>
+        <div className="answers-section">
+          <h3>Réponses :</h3>
+        </div>
+        <div className="answer-container"> 
+            {question.answers.map(answer => (
+              <p key={answer.id} className="answer-item">
+                <p className="answer-body">{answer.body}</p>
+              </p>
+            ))}
+        </div>
+      </div>
+    </>
   );
 }
 
