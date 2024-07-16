8343545f4526f846031ea9a64df1bde71079a088
ibrasow5 <111022844+ibrasow5@users.noreply.github.com>
2024-07-16 16:21:45 +0000

    Commit

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Logo from '../Tola5.png';
+import '../css/Dashboard.css';
 
 function QuestionDetail() {
   const { id } = useParams();
